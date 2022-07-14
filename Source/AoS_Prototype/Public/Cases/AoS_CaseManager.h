72f22a7d13ab6f7dacd1abdc96d0377e757538ee
Little Grim Entertainment <info@littlegriment.com>
2022-07-13 23:26:40 -0500

    Adding new case manager as game instance subsystem

---
+++ Source/AoS_Prototype/Public/Cases/AoS_CaseManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "AoS_CaseManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AOS_PROTOTYPE_API UAoS_CaseManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
