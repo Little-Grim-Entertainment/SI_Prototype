afc718ba4484bcb58e742005f6f2a9b3c1d77bca
Little Grim Entertainment <info@littlegriment.com>
2022-08-15 22:46:59 -0500

    Begin work on TimeManager system for day/night cycle and time of day

---
+++ Source/AoS_Prototype/Private/World/AoS_TimeManager.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "World/AoS_TimeManager.h"
+
+UAoS_TimeManager::UAoS_TimeManager()
+{
+	
+}
---
+++ Source/AoS_Prototype/Public/World/AoS_TimeManager.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "AoS_TimeManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AOS_PROTOTYPE_API UAoS_TimeManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+
+	UAoS_TimeManager();
+};
