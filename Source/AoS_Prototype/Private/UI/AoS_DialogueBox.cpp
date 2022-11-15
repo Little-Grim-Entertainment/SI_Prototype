7bc2e48bf2508a3906ea0d61ce262ff733f62539
littlegrim <66577696+littlegrim@users.noreply.github.com>
2022-11-14 21:37:11 -0600

    Added delegates to Dialogue box for clicking Next/Previous

--- Source/AoS_Prototype/Private/UI/AoS_DialogueBox.cpp
+++ Source/AoS_Prototype/Private/UI/AoS_DialogueBox.cpp
@@ -3,7 +3,42 @@
 
 #include "UI/AoS_DialogueBox.h"
 #include "DlgContext.h"
+#include "Components/Button.h"
 #include "Components/TextBlock.h"
+#include "Dialogue/AoS_DialogueManager.h"
+
+
+void UAoS_DialogueBox::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (!BTN_Next || !BTN_Previous){return;}
+
+	UAoS_DialogueManager* DialogueManager = GetWorld()->GetSubsystem<UAoS_DialogueManager>();
+	if (!IsValid(DialogueManager)){return;}
+	
+	BTN_Next->OnClicked.AddDynamic(this, &ThisClass::OnNextClicked);
+	BTN_Next->OnHovered.AddDynamic(this, &ThisClass::OnNextHovered);
+	BTN_Next->OnUnhovered.AddDynamic(this, &ThisClass::OnNextUnhovered);
+
+	BTN_Previous->OnClicked.AddDynamic(this, &ThisClass::OnPreviousClicked);
+	BTN_Previous->OnHovered.AddDynamic(this, &ThisClass::OnPreviousHovered);
+	BTN_Previous->OnUnhovered.AddDynamic(this, &ThisClass::OnPreviousUnhovered);
+
+	//OnNextClickedDelegate.AddDynamic(DialogueManager, &UAoS_DialogueManager::OnNextPressed);
+	//OnPreviousClickedDelegate.AddDynamic(DialogueManager, &UAoS_DialogueManager::OnPreviousPressed);
+}
+
+void UAoS_DialogueBox::NativeDestruct()
+{
+	Super::NativeDestruct();
+	
+	UAoS_DialogueManager* DialogueManager = GetWorld()->GetSubsystem<UAoS_DialogueManager>();
+	if (!IsValid(DialogueManager)){return;}
+	
+	//OnNextClickedDelegate.RemoveDynamic(DialogueManager, &UAoS_DialogueManager::OnNextPressed);
+	//OnPreviousClickedDelegate.RemoveDynamic(DialogueManager, &UAoS_DialogueManager::OnPreviousPressed);
+}
 
 void UAoS_DialogueBox::SetCharacterName(FText InCharacterName)
 {
@@ -44,3 +79,64 @@ FText UAoS_DialogueBox::GetCurrentDialogue() const
 {
 	return CurrentDialogue;
 }
+
+FOnNextClickedDelegate& UAoS_DialogueBox::GetOnNextClickedDelegate()
+{
+	return OnNextClickedDelegate;
+}
+
+FOnNextHoveredDelegate& UAoS_DialogueBox::GetOnNextHoveredDelegate()
+{
+	return OnNextHoveredDelegate;
+}
+
+FOnNextUnhoveredDelegate& UAoS_DialogueBox::GetOnNextUnhoveredDelegate()
+{
+	return OnNextUnhoveredDelegate;
+}
+
+FOnPreviousClickedDelegate& UAoS_DialogueBox::GetOnPreviousClickedDelegate()
+{
+	return OnPreviousClickedDelegate;
+}
+
+FOnPreviousHoveredDelegate& UAoS_DialogueBox::GetOnPreviousHoveredDelegate()
+{
+	return OnPreviousHoveredDelegate;
+}
+
+FOnPreviousUnhoveredDelegate& UAoS_DialogueBox::GetOnPreviousUnhoveredDelegate()
+{
+	return OnPreviousUnhoveredDelegate;
+}
+
+void UAoS_DialogueBox::OnNextClicked()
+{
+	OnNextClickedDelegate.Broadcast();
+}
+
+void UAoS_DialogueBox::OnNextHovered()
+{
+	OnNextHoveredDelegate.Broadcast();
+}
+
+void UAoS_DialogueBox::OnNextUnhovered()
+{
+	OnNextUnhoveredDelegate.Broadcast();
+}
+
+void UAoS_DialogueBox::OnPreviousClicked()
+{
+	OnPreviousClickedDelegate.Broadcast();
+}
+
+void UAoS_DialogueBox::OnPreviousHovered()
+{
+	OnPreviousHoveredDelegate.Broadcast();
+}
+
+void UAoS_DialogueBox::OnPreviousUnhovered()
+{
+	OnPreviousUnhoveredDelegate.Broadcast();
+}
+
--- Source/AoS_Prototype/Public/UI/AoS_DialogueBox.h
+++ Source/AoS_Prototype/Public/UI/AoS_DialogueBox.h
@@ -6,8 +6,18 @@
 #include "UI/AoS_UserWidget.h"
 #include "AoS_DialogueBox.generated.h"
 
+class UButton;
 class UTextBlock;
 class UDlgContext;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnNextClickedDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnNextHoveredDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnNextUnhoveredDelegate);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPreviousClickedDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPreviousHoveredDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPreviousUnhoveredDelegate);
+
 
 UCLASS()
 class AOS_PROTOTYPE_API UAoS_DialogueBox : public UAoS_UserWidget
@@ -26,6 +36,14 @@ public:
 	FText GetCurrentSpeaker() const;
 	UFUNCTION(BlueprintCallable)
 	FText GetCurrentDialogue() const;
+
+	FOnNextClickedDelegate& GetOnNextClickedDelegate();
+	FOnNextHoveredDelegate& GetOnNextHoveredDelegate();
+	FOnNextUnhoveredDelegate& GetOnNextUnhoveredDelegate();
+
+	FOnPreviousClickedDelegate& GetOnPreviousClickedDelegate();
+	FOnPreviousHoveredDelegate& GetOnPreviousHoveredDelegate();
+	FOnPreviousUnhoveredDelegate& GetOnPreviousUnhoveredDelegate();
 	
 protected:
 
@@ -33,10 +51,39 @@ protected:
 	UTextBlock* TXT_CharacterName;
 	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
 	UTextBlock* TXT_CharacterDialogue;
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UButton* BTN_Next;
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UButton* BTN_Previous;
+
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
 
 private:
 	
 	FText CurrentSpeaker;
 	FText CurrentDialogue;
-	
+
+	FOnNextClickedDelegate OnNextClickedDelegate;
+	FOnNextHoveredDelegate OnNextHoveredDelegate;
+	FOnNextUnhoveredDelegate OnNextUnhoveredDelegate;
+
+	FOnPreviousClickedDelegate OnPreviousClickedDelegate;
+	FOnPreviousHoveredDelegate OnPreviousHoveredDelegate;
+	FOnPreviousUnhoveredDelegate OnPreviousUnhoveredDelegate;
+
+	UFUNCTION()
+	void OnNextClicked();
+	UFUNCTION()
+	void OnNextHovered();
+	UFUNCTION()
+	void OnNextUnhovered();
+
+	UFUNCTION()
+	void OnPreviousClicked();
+	UFUNCTION()
+	void OnPreviousHovered();
+	UFUNCTION()
+	void OnPreviousUnhovered();
+
 };
