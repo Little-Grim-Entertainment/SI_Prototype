3b4ab161eb18cee9da587f152d62c544cbb7cc9b
Zack Tripp <trippzac@gmail.com>
2022-11-17 20:17:44 -0600

    Old dialogue references removed, new dialogue classes added
    To do:
    Add editor module for the new dialogue classes

---
+++ Source/AoS_Prototype/Public/Dialogue/DialogueSessionEdge.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GenericGraphEdge.h"
+#include "DialogueSessionEdge.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AOS_PROTOTYPE_API UDialogueSessionEdge : public UGenericGraphEdge
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/AoS_Prototype/Public/Dialogue/DialogueSessionNode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GenericGraphNode.h"
+#include "DialogueSessionNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AOS_PROTOTYPE_API UDialogueSessionNode : public UGenericGraphNode
+{
+	GENERATED_BODY()
+	
+};
