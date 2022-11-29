ae9960bc09aeacc8aeb8160caeab03336be43ec7
littlegrim <66577696+littlegrim@users.noreply.github.com>
2022-11-29 16:36:44 -0600

    Character Interaction system updates

--- Source/AoS_Prototype/Public/Characters/AoS_NPC_Interactable.h
+++ Source/AoS_Prototype/Public/Characters/AoS_NPC_Interactable.h
@@ -4,13 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "Characters/AoS_NPC.h"
+#include "Interfaces/AoS_InteractInterface.h"
 #include "AoS_NPC_Interactable.generated.h"
 
-/**
- * 
- */
+
+class AAoS_Nick;
+class UAoS_InteractableComponent;
+
 UCLASS()
-class AOS_PROTOTYPE_API AAoS_NPC_Interactable : public AAoS_NPC
+class AOS_PROTOTYPE_API AAoS_NPC_Interactable : public AAoS_NPC, public IAoS_InteractInterface
 {
 	GENERATED_BODY()
 	
@@ -30,16 +32,24 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CharacterInfo")
 	FText CharacterName;	
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Interaction")
-	class UAoS_InteractableComponent* InteractableComponent;
-	UPROPERTY(EditDefaultsOnly, Category = "Interaction")
-	class UBoxComponent* OverlapBox;
+	UAoS_InteractableComponent* InteractableComponent;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly,  Category = "Interaction")
-	class UWidgetComponent* InteractionIcon;
+	UWidgetComponent* InteractionIcon;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Interaction")
-	class UWidgetComponent* InteractionPrompt;
+	UWidgetComponent* InteractionPrompt;
 	
 protected:
 
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnBeginOverlap(AAoS_Nick* InNickActor);
+	UFUNCTION()
+	void OnEndOverlap(AAoS_Nick* InNickActor);
+	
+	UFUNCTION()
+	virtual UWidgetComponent* GetInteractionIconComponent_Implementation() override;
+	UFUNCTION()
+	virtual UWidgetComponent* GetInteractionPromptComponent_Implementation() override;
 	
 };
