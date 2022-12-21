1b84323ad9558de9150cdc310f45cd758526c089
Liam Snow <61256485+FrostDrake@users.noreply.github.com>
2022-12-21 00:14:29 +0000

    Revert "Dealing with merge conflict"
    This reverts commit 200f147f2c3eeaf3e37460a7c95342879780d40d.

--- Source/AoS_Prototype/Public/Characters/AoS_Nick.h
+++ Source/AoS_Prototype/Public/Characters/AoS_Nick.h
@@ -9,12 +9,9 @@
 class UAoS_NickCharacterData;
 class USpringArmComponent;
 class UCameraComponent;
-<<<<<<< HEAD
 class UAoS_AIPerceptionStimuliSource;
-=======
 class UAoS_LevelManager;
 class UAoS_MapData;
->>>>>>> cce4cdcfa9286d95bddaf46848769a82dd2c851b
 
 UCLASS()
 class AOS_PROTOTYPE_API AAoS_Nick : public AAoS_Character
@@ -31,14 +28,12 @@ private:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, NoClear, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* CameraBoom;
-<<<<<<< HEAD
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, NoClear, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FollowCamera;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, NoClear, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* ObservationCamera;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, NoClear, Category = AI, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UAoS_AIPerceptionStimuliSource> PerceptionStimuliSourceComponent = nullptr;
-=======
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UChildActorComponent* FollowCamera;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
@@ -52,7 +47,6 @@ private:
 	
 	UPROPERTY()
 	UAoS_LevelManager* LevelManager;
->>>>>>> cce4cdcfa9286d95bddaf46848769a82dd2c851b
 		
 	// ================== FUNCTIONS ==================
 
@@ -76,17 +70,14 @@ public:
 	/** Returns FollowCamera subobject **/
 	FORCEINLINE UChildActorComponent* GetFollowCamera() const { return FollowCamera; }
 	/** Returns ObservationCamera subobject **/
-<<<<<<< HEAD
 	FORCEINLINE UCameraComponent* GetObservationCamera() const { return ObservationCamera; }
 	/** Returns PerceptionStimuliSourceComponent subobject **/
 	FORCEINLINE UAoS_AIPerceptionStimuliSource* GetPerceptionStimuliSource() const { return PerceptionStimuliSourceComponent; }
-=======
 	FORCEINLINE UChildActorComponent* GetObservationCamera() const { return ObservationCamera; }
 	/** Returns FollowCamera Actor **/
 	FORCEINLINE ACameraActor* GetFollowCameraActor() const { return FollowCameraActor; }
 	/** Returns ObservationCamera Actor **/
 	FORCEINLINE ACameraActor* GetObservationCameraActor() const { return ObservationCameraActor; }
->>>>>>> cce4cdcfa9286d95bddaf46848769a82dd2c851b
 	
 protected:
 	virtual void BeginPlay() override;
