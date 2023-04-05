d412cc240cdfe673bfb391ce1d1333576099c4de
Little Grim Entertainment <66577696+littlegrim@users.noreply.github.com>
2023-04-05 16:07:17 -0500

    Cleaning up MapData merge

--- Source/AoS_Prototype/Private/Data/Maps/AoS_MapData.cpp
+++ Source/AoS_Prototype/Private/Data/Maps/AoS_MapData.cpp
@@ -2,17 +2,3 @@
 
 
 #include "Data/Maps/AoS_MapData.h"
-
-/*void UAoS_MapData::PlayLevelBackgroundMusic(UAoS_MusicManager* InMusicManager)
-{
-	if (!IsValid(InMusicManager)) {return;}
-	
-	if (BackgroundMusicSettings.bHasIntro)
-	{
-		InMusicManager->PlayBackgroundMusicLoopWithIntro(BackgroundMusicSettings);
-	}
-	else
-	{
-		InMusicManager->PlayBackgroundMusic(BackgroundMusicSettings);
-	}
-}*/
