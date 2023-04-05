f7161d63635540b7483a0878ba746ca8fe1fbe45
Little Grim Entertainment <66577696+littlegrim@users.noreply.github.com>
2023-04-05 15:03:54 -0500

    Tag updates

---
+++ Source/AoS_Prototype/Private/Levels/AoS_MapGameplayTagLibrary.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Levels/AoS_MapGameplayTagLibrary.h"
+
+namespace AoS_MapGameplayTagLibrary
+{
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map, "Map")
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Type, "Map.Type")
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Type_Interior, "Map.Type.Interior")
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Type_Exterior, "Map.Type.Exterior")
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Type_Menu, "Map.Type.Menu")
+
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Title, "Map.Title")
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Title_NovaLuxDistrict, "Map.Title.NovaLuxDistrict")
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Title_NovaLuxDistrict_NicksAppartment, "Map.Title.NovaLuxDistrict.NicksAppartment")
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Title_NovaLuxDistrict_BettyBots, "Map.Title.NovaLuxDistrict.BettyBots")
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Title_NovaLuxDistrict_CherryTree, "Map.Title.NovaLuxDistrict.CherryTree")
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Title_Menu_MainMenu, "Map.Title.NovaLuxDistrict.MainMenu")
+
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Title_Test, "Map.Title.Test")
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Title_Test_Bar, "Map.Title.Test.Bar")
+	UE_DEFINE_GAMEPLAY_TAG(AOSTag_Map_Title_Test_Sandbox, "Map.Title.Test.Sandbox")
+}
