24e006e189ac390c2f77405f612f4b52546c82b3
Little Grim Entertainment <66577696+littlegrim@users.noreply.github.com>
2023-04-25 14:28:35 -0500

    Revert "pull from main"
    This reverts commit edad73d7570cae0ce4e3b80bac841edbb03a0fae.

--- Plugins/AdvancedThirdPersonCamera/Source/AdvancedThirdPersonCamera/Private/ATPCLockOnTargetLocationComponent.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 Alexander Shumeyko. All Rights Reserved.
-
-#include "ATPCLockOnTargetLocationComponent.h"
-
-FVector UATPCLockOnTargetLocationComponent::GetTargetLocation()
-{
-	return GetComponentLocation();
-}
