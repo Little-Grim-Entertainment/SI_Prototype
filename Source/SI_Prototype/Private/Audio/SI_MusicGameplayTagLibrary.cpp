6f87b2baa5b74280a087ac4a5199ce87f4bd52f5
littlegrim <66577696+littlegrim@users.noreply.github.com>
2023-08-04 19:44:40 -0500

    Added in music audio tags

---
+++ Source/SI_Prototype/Private/Audio/SI_MusicGameplayTagLibrary.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Audio/SI_MusicGameplayTagLibrary.h"
+
+namespace SI_MusicGameplayTagLibrary
+{
+	UE_DEFINE_GAMEPLAY_TAG(SITag_Audio_Music_BigShots, "Audio.Music.BigShots")
+	UE_DEFINE_GAMEPLAY_TAG(SITag_Audio_Music_BotsForSale, "Audio.Music.BotsForSale")
+	UE_DEFINE_GAMEPLAY_TAG(SITag_Audio_Music_BustleBound, "Audio.Music.BustleBound")
+	UE_DEFINE_GAMEPLAY_TAG(SITag_Audio_Music_ForOurEsteemedGuests, "Audio.Music.ForOurEsteemedGuests")
+	UE_DEFINE_GAMEPLAY_TAG(SITag_Audio_Music_LikeClockwork, "Audio.Music.LikeClockwork")
+	UE_DEFINE_GAMEPLAY_TAG(SITag_Audio_Music_MainMenu, "Audio.Music.MainMenu")
+	UE_DEFINE_GAMEPLAY_TAG(SITag_Audio_Music_MyLovelies, "Audio.Music.MyLovelies")
+	UE_DEFINE_GAMEPLAY_TAG(SITag_Audio_Music_NightOwl, "Audio.Music.NightOwl")
+	UE_DEFINE_GAMEPLAY_TAG(SITag_Audio_Music_OldTime, "Audio.Music.OldTime")
+	UE_DEFINE_GAMEPLAY_TAG(SITag_Audio_Music_PocketAce, "Audio.Music.PocketAce")
+	UE_DEFINE_GAMEPLAY_TAG(SITag_Audio_Music_PunchingIn, "Audio.Music.PunchingIn")
+}
