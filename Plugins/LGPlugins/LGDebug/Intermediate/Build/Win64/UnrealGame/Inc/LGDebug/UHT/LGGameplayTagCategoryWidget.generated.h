065f7dad2137c1a82e80c1e61e39bf7f395dafdd
Little Grim Entertainment <info@littlegriment.com>
2023-11-06 14:50:28 -0600

    Added LGPlugins folder

---
+++ Plugins/LGPlugins/LGDebug/Intermediate/Build/Win64/UnrealGame/Inc/LGDebug/UHT/LGGameplayTagCategoryWidget.generated.h
@@ -0,0 +1,72 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "UI/GameplayTags/LGGameplayTagCategoryWidget.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+struct FGameplayTag;
+#ifdef LGDEBUG_LGGameplayTagCategoryWidget_generated_h
+#error "LGGameplayTagCategoryWidget.generated.h already included, missing '#pragma once' in LGGameplayTagCategoryWidget.h"
+#endif
+#define LGDEBUG_LGGameplayTagCategoryWidget_generated_h
+
+#define FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_SPARSE_DATA
+#define FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execHasTagEntry); \
+	DECLARE_FUNCTION(execAddTagEntry);
+
+
+#define FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_ACCESSORS
+#define FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesULGGameplayTagCategoryWidget(); \
+	friend struct Z_Construct_UClass_ULGGameplayTagCategoryWidget_Statics; \
+public: \
+	DECLARE_CLASS(ULGGameplayTagCategoryWidget, UUserWidget, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/LGDebug"), NO_API) \
+	DECLARE_SERIALIZER(ULGGameplayTagCategoryWidget)
+
+
+#define FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ULGGameplayTagCategoryWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ULGGameplayTagCategoryWidget(ULGGameplayTagCategoryWidget&&); \
+	NO_API ULGGameplayTagCategoryWidget(const ULGGameplayTagCategoryWidget&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ULGGameplayTagCategoryWidget); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ULGGameplayTagCategoryWidget); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ULGGameplayTagCategoryWidget) \
+	NO_API virtual ~ULGGameplayTagCategoryWidget();
+
+
+#define FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_14_PROLOG
+#define FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_SPARSE_DATA \
+	FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_ACCESSORS \
+	FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_INCLASS_NO_PURE_DECLS \
+	FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h_17_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> LGDEBUG_API UClass* StaticClass<class ULGGameplayTagCategoryWidget>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_SI_Prototype_Plugins_LGDebug_Source_LGDebug_Public_UI_GameplayTags_LGGameplayTagCategoryWidget_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
