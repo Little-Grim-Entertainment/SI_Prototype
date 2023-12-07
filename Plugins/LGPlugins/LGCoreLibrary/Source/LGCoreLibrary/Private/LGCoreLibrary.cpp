6912af22c6c05cd199e2e4ba9e4a59f191b65328
Little Grim Entertainment <info@littlegriment.com>
2023-12-07 14:56:32 -0600

    Adding in LGCoreLibrary plugin

---
+++ Plugins/LGPlugins/LGCoreLibrary/Source/LGCoreLibrary/Private/LGCoreLibrary.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LGCoreLibrary.h"
+
+#define LOCTEXT_NAMESPACE "FLGCoreLibraryModule"
+
+void FLGCoreLibraryModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FLGCoreLibraryModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FLGCoreLibraryModule, LGCoreLibrary)
