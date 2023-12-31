25854a317248da41f6a7888ecce9d7bf441e3eb6
Little Grim Entertainment <info@littlegriment.com>
2023-12-31 15:45:14 -0600

    Adding an SI_PrimaryDataAsset class

---
+++ Source/SI_Prototype/Public/SI_PrimaryDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "SI_PrimaryDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SI_PROTOTYPE_API USI_PrimaryDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
